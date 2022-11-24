2c338ea621d9133dccb8fc9fe2cd8b38359f17b5
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-23 22:19:01 -0500

    commit tree bd997ce2b2f02ed8343dc8bd6639429b26f1387c

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,7 @@
 def admin_login(username, password)
-  # your code here
+    # your code here
+    if (username == "admin" || username == "ADMIN") && (password == "12345")
+      "Access granted"
 end
 
 def hows_the_weather(temperature)
